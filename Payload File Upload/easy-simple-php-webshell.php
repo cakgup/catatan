c843e5bd531a5182979a6e3f346883055b78a0d6
Mukh. Gufron Ikhsan <mukh.gufron@ui.ac.id>
2026-09-12 10:38:04 +0700

    update

---
+++ Payload File Upload/easy-simple-php-webshell.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<form method="GET" name="easy-simple-php-webshell.php">
+<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
+</pre>
+</body>
+</html>
